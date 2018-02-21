04b14450f42b8bd9ff64812b7ba66947f52acf30
holsingc8877 <36680608+holsingc8877@users.noreply.github.com>
2018-02-20 21:17:29 -0500

    Add files via upload
    First Submission

---
+++ P2T1_SalesPrediction_HolsingerChris.py
@@ -0,0 +1,12 @@
+# CTI-110 
+# P2T1 - Sales Prediction 
+# Chris Holsinger
+# 18 Feb 2018
+
+#This program is designed to determine the annual profit expected
+
+totalSales=float(input ("Enter the annual projected total sales"))
+
+profit=totalSales * .23
+
+print ("Your profits for the year, $",format (profit,'.2f'),sep='')
